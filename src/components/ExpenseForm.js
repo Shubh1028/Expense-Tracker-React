b71baecfe984d0788109f8e29374386fc0924f8a
Shubh <shubh410shekhar@gmail.com>
2022-07-27 16:42:16 +0530

    Form Submission

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -15,12 +15,23 @@ function ExpenseForm() {
     const dateHandler = (event) => {
         setEnteredDate(event.target.value)
     }
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+
+        console.log(expenseData);
+    }
     return (
-        <form>
+        <form onSubmit={submitForm}>
             <input type='text' placeholder="Enter Title" onChange={titleChangeHandler}/>
             <input type='number' placeholder="Enter Amount" onChange={amountHandler}/>
             <input type='date' onChange={dateHandler}/>
-            <button type='submit'>Add Expense</button>
+            <button type='submit' >Add Expense</button>
         </form>
     )
 }
