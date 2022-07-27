f34e833111a1ff1aca2fa62aa6c7ce4f1ae12c3e
Shubh <shubh410shekhar@gmail.com>
2022-07-27 13:33:09 +0530

    DOM Deletion

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,11 +3,19 @@ import ExpenseDate from './ExpenseDate';
 import ExpenseDetail from './ExpenseDetails';
 
 function ExpenseItem(props) {
+
+  function deleteExpense() {
+    const element = document.getElementById(props.title);
+
+    element.remove();
+
+  }
   
   return (
-    <div className='expense-item'>
+    <div className='expense-item' id={props.title}>
     <ExpenseDate date = {props.date} />
     <ExpenseDetail title={props.title} amount = {props.amount}/>
+    <button onClick={deleteExpense}>Delete</button>
     </div>
   );
 }
