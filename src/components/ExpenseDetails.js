0571805b75641d90514b57305592faf36391855e
Shubh <shubh410shekhar@gmail.com>
2022-07-27 14:17:54 +0530

    useState Manipulation

--- src/components/ExpenseDetails.js
+++ src/components/ExpenseDetails.js
@@ -1,11 +1,19 @@
+import React, { useState } from 'react';
 import './ExpenseItem.css'
 
 function ExpenseDetail(props) {
 
+  const [expense, setExpense] = useState(props.amount)
+
+  const addExpense = () => {
+    setExpense(100);
+  }
+
     return (
          <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>{props.amount}</div>
+        <div className='expense-item__price'>{expense}</div>
+        <button onClick={addExpense}>Add Expense</button>
       </div> 
 
     )
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,19 +3,10 @@ import ExpenseDate from './ExpenseDate';
 import ExpenseDetail from './ExpenseDetails';
 
 function ExpenseItem(props) {
-
-  function deleteExpense() {
-    const element = document.getElementById(props.title);
-
-    element.remove();
-
-  }
-  
   return (
-    <div className='expense-item' id={props.title}>
+    <div className='expense-item'>
     <ExpenseDate date = {props.date} />
     <ExpenseDetail title={props.title} amount = {props.amount}/>
-    <button onClick={deleteExpense}>Delete</button>
     </div>
   );
 }
